6641bf8c3de321fe57935b5b800b86db881a8159
Trevor Lesley <tlesley96@gmail.com>
2021-06-11 00:47:08 -0400

    Updating progress, not finished yet.

--- act_4.py
+++ act_4.py
@@ -18,5 +18,24 @@ slowprint(
 
 choice = input("1/2/3?")
 
-if '1' in choice:
-    
+if "1" in choice:
+    slowprint(
+        """Examiner didn’t like your tone, but he offers you gas money in exchange for another favor, much more serious in tone. He doesn’t tell you what the favor is unless you agree to it, so he gives you a moment to think it over. What do you do?"""
+    )
+    choice = input("Do the favor = y; Say no = n")
+
+    if "y" in choice:
+        slowprint(
+            """The examiner reveals that he's actually an alien, and he needs a human sacrifice; you've just been beamed up into a spaceship, never to be seen again. \n
+        Game over."""
+        )
+
+    if "n" in choice:
+        slowprint(
+            """The examiner shrugs his shoulders, seemingly unbothered, and decides to give you the money anyways. You head back to the DMV to get scored and discover if you passed or failed."""
+        )
+
+if "2" in choice:
+    slowprint(
+        """The man actually turns out to be a really nice guy, gives you twenty bucks and tells you next time he sees you he wants to offer you an.... opportunity. You fill up and head back to the DMV to finish your test. """
+    )
